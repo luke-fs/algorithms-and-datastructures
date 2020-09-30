a56901fd00d193fcaf248fc90b5428491fe581f2
Luke <luke.strauer1@googlemail.com>
2020-09-30 22:33:32 +0200

    started greedy algorithms with money change implementation

--- week3_greedy_algorithms/1_money_change/change.py
+++ week3_greedy_algorithms/1_money_change/change.py
@@ -3,8 +3,21 @@ import sys
 
 def get_change(m):
     #write your code here
-    return m
+    c = 0
+    while m >= 0:
+        if m-10 >= 0:
+            m -= 10
+            c += 1
+        elif m-5 >= 0:
+            m -= 5
+            c += 1
+        elif m-1 >= 0:
+            m -= 1
+            c += 1
+        else:
+            return c
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
+    #m = int(input())
     print(get_change(m))
